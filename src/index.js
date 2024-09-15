4d4e1b0a14381681fb44ad564060d4bf6e9ca52c
Your GitHub Username <reben80@gmail.com>
2024-09-15 17:13:57 -0400

    Update Vercel configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Server listening on ${port}`);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
